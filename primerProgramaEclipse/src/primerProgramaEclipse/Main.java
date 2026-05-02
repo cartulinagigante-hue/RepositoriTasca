69ca353139de2aa455f316a1f19b5891abef7cb4
Dani <danielgallegoprado@gmail.com>
2026-05-02 11:26:22 +0200

    millora codi

--- primerProgramaEclipse/src/primerProgramaEclipse/Main.java
+++ primerProgramaEclipse/src/primerProgramaEclipse/Main.java
@@ -7,9 +7,9 @@ public class Main {
         System.out.println("Nom i Cognoms: Daniel Gallego Prado");
         System.out.println("Presentació: Soc estudiant de programació i tinc de tornar a fer aquest treball");
         System.out.println("M'agraden les birres.");
+    //editttt
 
-
-        // "2+4" -> 2+4 (Al conncatnear dos numeros el resultat sera 2+4 ja que el tractem els digits dins de les cometes)
+        // "2+4" -> 2+4 (Al conncatnear dos numeros el resultat sera 2+4 ja que el tractem els digits dins de ljaes cometes)
         System.out.println("2+4"); 
         
         // "2"+"4" -> : 24 (Unira els dos textos, no els sumarà)
